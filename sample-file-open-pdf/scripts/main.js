0b9e499158393fa34d5e868693d2ad719e0cd19e
ikivanov@gmail.com <ikivanov@gmail.com>
2013-01-31 14:09:02 +0200

    sample app impleneted

--- sample-file-open-pdf/scripts/main.js
+++ sample-file-open-pdf/scripts/main.js
@@ -1,64 +1,27 @@
-// JavaScript Document
-// Wait for PhoneGap to load
 document.addEventListener("deviceready", onDeviceReady, false);
 
-// PhoneGap is ready
 function onDeviceReady() {
-    getLocation();
-    navigator.splashscreen.hide();
-}
-
-function getLocation() {
-    myNewFunction();
-}
-  
-function myNewFunction(){
-    navigator.geolocation.getCurrentPosition(onGeolocationSuccess, onGeolocationError);
-}
-  
-//=======================Say Hello (Page 1) Operations=======================//
-function sayHello() {
-    var sayHelloInputElem = document.getElementById('helloWorldInput');
-    var sayHelloTextElem = document.getElementById('helloWorldText');
-    var inputText = document.getElementById('txtName');
-    
-    sayHelloTextElem.innerHTML = 'Hello, ' + inputText.value + '!';
-    sayHelloTextElem.style.display = 'block';
-    sayHelloInputElem.style.display = 'none';
-}
-
-function sayHelloReset() {
-    var sayHelloInputElem = document.getElementById('helloWorldInput');
-    var sayHelloTextElem = document.getElementById('helloWorldText');
-    var inputText = document.getElementById('txtName');
-    
-    inputText.value = '';
-    sayHelloTextElem.style.display = 'none';
-    sayHelloInputElem.style.display = 'block';
-}
-
-//=======================Geolocation Operations=======================//
-// onGeolocationSuccess Geolocation
-function onGeolocationSuccess(position) {
-    // Use Google API to get the location data for the current coordinates
-    var geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    geocoder.geocode({ "latLng": latlng }, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            if ((results.length > 1) && results[1]) {
-                $("#myLocation").html(results[1].formatted_address);
-            }
-        }
+    $("#btnOpenPDF").click(function() {
+        var app = new Application();
+        app.Run();
     });
-    
-    // Use Google API to get a map of the current location
-    // http://maps.googleapis.com/maps/api/staticmap?size=280x300&maptype=hybrid&zoom=16&markers=size:mid%7Ccolor:red%7C42.375022,-71.273729&sensor=true
-    var googleApis_map_Url = 'http://maps.googleapis.com/maps/api/staticmap?size=300x300&maptype=hybrid&zoom=16&sensor=true&markers=size:mid%7Ccolor:red%7C' + latlng;
-    var mapImg = '<img src="' + googleApis_map_Url + '" />';
-    $("#map_canvas").html(mapImg);
 }
 
-// onGeolocationError Callback receives a PositionError object
-function onGeolocationError(error) {
-    $("#myLocation").html("<span class='err'>" + error.message + "</span>");
+function Application() {
+}
+
+Application.prototype.Run = function() {
+    debugger;
+    if (device.platform == 'iPad' || device.platform == 'iPhone') {
+        var url = window.location.href.replace('index.html', 'sample.pdf');
+        window.plugins.childBrowser.showWebPage(url);
+    } else if (device.platform == 'Android') {
+        var path = "file://" + this.getWorkingFolder() + "sample.pdf";
+        window.plugins.childBrowser.openExternal(path);
+    }
+}
+
+Application.prototype.getWorkingFolder = function() {
+    var path = window.location.href.replace('index.html', '').replace('http://', '');
+    return path;
 }
