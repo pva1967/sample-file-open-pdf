e2641692df691cd4bf36e4888fb55348ec13d37b
Йордан Илчев <jordan@ilchev.com>
2013-02-01 01:41:46 -0600

    Note for pdf reader added
    + display local file path

--- sample-file-open-pdf/scripts/main.js
+++ sample-file-open-pdf/scripts/main.js
@@ -1,28 +1,35 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 //Activate :active state
-document.addEventListener("touchstart", function() {}, false);
+document.addEventListener("touchstart", function() {
+}, false);
 
 function onDeviceReady() {
-    document.getElementById('btnOpenPDF').onclick = function() {
-        var app = new Application();
-        app.Run();
-    }
+	document.getElementById('btnOpenPDF').onclick = function() {
+		var app = new Application();
+		app.Run();
+	}
 }
 
 function Application() {
 }
 
 Application.prototype.Run = function() {
-    if (device.platform === 'Android') {
-        var path = "file://" + this.getWorkingFolder() + "sample.pdf";
-        window.plugins.childBrowser.openExternal(path);
-    } else {
-        var url = window.location.href.replace('index.html', 'sample.pdf');
-        window.plugins.childBrowser.showWebPage(url);
-    }
+	var infoDiv = document.getElementById("infoField");
+    
+	if (device.platform === 'Android') {
+		var path = this.getWorkingFolder().replace('http://', 'file://') + "sample.pdf";
+
+		infoDiv.innerText = path;
+		window.plugins.childBrowser.openExternal(path);
+	} else {
+		var url = window.location.href.replace('index.html', 'sample.pdf');
+        
+		infoDiv.innerText = url;
+		window.plugins.childBrowser.showWebPage(url);
+	}
 }
 
 Application.prototype.getWorkingFolder = function() {
-    var path = window.location.href.replace('index.html', '').replace('http://', '');
-    return path;
+	var path = window.location.href.replace('index.html', '');
+	return path;
 }
