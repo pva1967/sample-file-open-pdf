f602360b44aa114017939e044acfa23c2e4ea474
Lyubomir Gospodinov <gospodinov@telerik.com>
2014-09-16 16:47:04 +0300

    Latest fix from gospodinov/fixExtractPdf
    Fix Android copying pdf file to public folder and then load it
    
    Fixes #258551
    http://teampulse.telerik.com/view#item/258551
    
    tested on Nexus5 and iPhone5

--- sample-file-open-pdf/scripts/main.js
+++ sample-file-open-pdf/scripts/main.js
@@ -1,37 +1,85 @@
- document.addEventListener("deviceready", onDeviceReady, false);
 //Activate :active state
 document.addEventListener("touchstart", function() {}, false);
 
+document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
-	navigator.splashscreen.hide();
-	document.getElementById('btnOpenPDF').onclick = function(){
+	document.getElementById('btnOpenPDF').onclick = function() {
 		var app = new Application();
 		app.run();
 	}
+
+	navigator.splashscreen.hide();
 }
 
 function Application() {
 }
 
-Application.prototype ={
-	run : function(){
-		var path,
-			windowTarget,
-			infoDiv = document.getElementById("infoField");
+Application.prototype = {
+	run : function() {
+		var pdfFileName = "sample.pdf",
+			samplePdfUrl;
 
 		if (window.navigator.simulator === true) {
 			alert("Not Supported in Simulator.");
 		} else {
-			windowTarget = device.platform.toLowerCase() === "ios" ? "_blank" : "_system";
-			path = infoDiv.innerText = this.getFilePath("sample.pdf", windowTarget);
-			window.open(path, windowTarget, "location=yes,hidden=no");
+			if (device.platform.toLowerCase() === "android") {
+				// file:///storage/emulated/0/Android/data/<app-id>/files/
+				// Directory has to be public, for the default pdf viewer to read it.
+				samplePdfUrl = cordova.file.externalDataDirectory + pdfFileName;
+
+				// Check whether the sample PDF file exists.
+				window.resolveLocalFileSystemURL(
+					samplePdfUrl,
+					loadPdfFromFileEntry,
+					function (error) {
+						copyFile(
+							getWorkingFolderFileURL(pdfFileName),
+							samplePdfUrl,
+							loadPdfFromFileEntry,
+							function(error) {
+								alert("Error copying file.");
+								console.log("Error: " + JSON.stringify(error, null, 4));
+							});
+					});
+			} else {
+				samplePdfUrl = getWorkingFolderFileURL(pdfFileName);
+				loadPdf(samplePdfUrl);
+			}
 		}
 	},
-	getFilePath: function(filePath, windowTarget){
-		return this.getWorkingFolder().replace("http://", "file://") + filePath;
-	},
-	getWorkingFolder : function() {
-		var indexUrl = window.location.href;
-		return indexUrl.substring(0, indexUrl.indexOf("index.html"));
-	}
+}
+
+function getWorkingFolderURL() {
+	var indexUrl = window.location.href;
+	return indexUrl.substring(0, indexUrl.indexOf("index.html"));
+}
+
+function getWorkingFolderFileURL(filePath) {
+	return getWorkingFolderURL().replace("http://", "file://") + filePath;
+}
+
+function loadPdf(targetUrl) {
+	var infoDiv = document.getElementById("infoField"),
+		windowTarget = device.platform.toLowerCase() === "ios" ? "_blank" : "_system";
+		
+	infoDiv.innerText = targetUrl;
+	console.log("Loading PDF file from: " + targetUrl);
+
+	window.open(targetUrl, windowTarget, "location=yes,hidden=no");
+}
+
+function loadPdfFromFileEntry(fileEntry) {
+	loadPdf(fileEntry.toURL());	
+}
+
+function copyFile(sourceUri, targetUri, successFunction, errorFunction) {
+	var fileTransfer = new FileTransfer();
+	
+	console.log("Copying PDF file from: " + sourceUri + " to: " + targetUri);	
+
+	fileTransfer.download(
+		encodeURI(sourceUri),
+		encodeURI(targetUri),
+		successFunction,
+		errorFunction);
 }
